214f65cd9f7ecde4a5d587060d7bf658b4a1d80d
Samantha Murera Kayitete <86477268+Samiskina@users.noreply.github.com>
2022-07-19 14:36:56 +0200

    better

--- src/components/Product_details.js
+++ src/components/Product_details.js
@@ -7,7 +7,7 @@ import UseFetch from '../helpers/getProductById';
   const params=useParams()
   const [user, setUser] = React.useState([])
   const {data} = UseFetch(`https://first-shopping-cart-api.herokuapp.com/products/${params.product_id}`);
-  
+  console.log(data, "=====");
   const [cartItems, setCartItems] = useState(() => {
     const localData = localStorage.getItem('cartItems');
     return localData ? JSON.parse(localData) : [];
